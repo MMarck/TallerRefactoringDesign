00f9f10190b77d13eb0ef45da9763476e32d8438
Joyce Rojas <72813269+JoyceRojas@users.noreply.github.com>
2021-01-07 13:24:27 -0500

    Cambio en Materia y Estudiante

--- src/modelos/Materia.java
+++ src/modelos/Materia.java
@@ -1,11 +1,61 @@
 package modelos;
 
 public class Materia {
-    public String codigo;
-    public String nombre;
-    public String facultad;
-    public double notaInicial;
-    public double notaFinal;
-    public double notaTotal;
+    private String codigo;
+    private String nombre;
+    private String facultad;
+    private double notaInicial;
+    private double notaFinal;
+    private double notaTotal;  
+    
+    public String getCodigo(){
+        return codigo; 
+    } 
+    
+    public String getNombre(){
+        return nombre;
+    } 
+    
+    public String getFacultad(){
+        return facultad;
+    }  
+    
+    public double getNotaInicial(){
+        return notaInicial;
+    }  
+    
+    public double getNotaFinal(){
+        return notaFinal;
+    }  
+    
+    public double getNotatotal(){
+        return notaTotal;
+    }  
+    
+    public void setCodigo(){
+        this.codigo=codigo;
+    } 
+    
+    public void setNombre(){
+        this.nombre=nombre;
+    } 
+    
+    public void setFacultad(){
+        this.facultad=facultad;
+    } 
+    
+    public void setNotaInicial(){
+        this.notaInicial=notaInicial;
+    } 
+    
+    public void setNotaFinal(){
+        this.facultad=facultad;
+    } 
+    
+    public void setNotaTotal(){
+        this.notaTotal=notaTotal;
+    } 
+    
     
 }
+
